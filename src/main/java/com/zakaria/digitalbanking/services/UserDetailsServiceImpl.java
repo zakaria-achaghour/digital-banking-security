a27576df6e106582f195c8ce268589e91433fbae
Achaghour zakaria <achaghourzakaria@gmail.com>
2023-08-03 11:54:24 +0100

    auth roles authorities

--- src/main/java/com/zakaria/digitalbanking/services/UserDetailsServiceImpl.java
+++ src/main/java/com/zakaria/digitalbanking/services/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.zakaria.digitalbanking.services;
 
 import com.zakaria.digitalbanking.entities.AppUser;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,10 +20,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         AppUser  appUser =  appAcocountService.loadUserByUsername(username);
         if (appUser == null) throw new UsernameNotFoundException(String.format("User %s not found" , username));
-        String[] roles = appUser.getRoles().stream().map(r -> r.getName()).toArray(String[]::new);
+        //String[] roles = appUser.getRoles().stream().map(r -> r.getName()).toArray(String[]::new);
+        List<SimpleGrantedAuthority> authorities = appUser.getRoles().stream().map(r -> new SimpleGrantedAuthority(r.getName())).collect(Collectors.toList());
         UserDetails userDetails = User.withUsername(appUser.getUsername())
                 .password(appUser.getPassword())
-                .roles(roles).build();
+                .authorities(authorities)
+          //      .roles(roles)
+                .build();
         return userDetails;
     }
 }
